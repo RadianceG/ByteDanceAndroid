dd7451c4b61498055a0e9bafae882c58bfc210d6
RadianceG <1054120576@qq.com>
2020-07-08 00:14:22 +0800

    finish homework 2

---
+++ README.md
@@ -0,0 +1,6 @@
+# ByteDanceAndroid
+Homework for the summer class
+## Homework1
+采用了TextView，EditView，Processbar,ImageView,Button五个控件，可以使用Homework1.apk直接进行检测。
+## Homework2
+点击item跳转，点击查找子View可以查找recycler的子View，可以使用Homework2.apk直接进行检测。
--- homework2/app/src/main/java/com/example/recyclelist/MainActivity.java
+++ homework2/app/src/main/java/com/example/recyclelist/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.listviewtest.Fruit;
 
@@ -25,10 +29,33 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter=new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
+        LinearLayout fensi =(LinearLayout)findViewById(R.id.fensi);
+        fensi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"你没有粉丝",Toast.LENGTH_SHORT).show();
+            }
+        });
+        LinearLayout zan =(LinearLayout)findViewById(R.id.zan);
+        zan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"你也没有赞",Toast.LENGTH_SHORT).show();
+            }
+        });
+        LinearLayout pinglun =(LinearLayout)findViewById(R.id.pinglun);
+        pinglun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"recycler_view的总子View为  "+count1(findViewById(R.id.recycler_view)),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
     private void initFruits()
     {
-        for (int i=0;i<2;i++)
+        for (int i=0;i<4;i++)
         {
             Fruit apple=new Fruit("Apple",R.drawable.apple_pic);
             fruitList.add(apple);
@@ -39,5 +66,26 @@ public class MainActivity extends AppCompatActivity {
             Fruit orange=new Fruit("orange",R.drawable.orange_pic);
             fruitList.add(orange);
         }
+    }
+    public int count1(View root) {
+        int viewCount = 0;
+
+        if (null == root) {
+            return 0;
+        }
+
+        if (root instanceof ViewGroup) {
+            viewCount++;
+            for (int i = 0; i < ((ViewGroup) root).getChildCount(); i++) {
+                View view = ((ViewGroup) root).getChildAt(i);
+                if (view instanceof ViewGroup) {
+                    viewCount += count1(view);
+                } else {
+                    viewCount++;
+                }
+            }
+        }
+
+        return viewCount;
     }
 }
